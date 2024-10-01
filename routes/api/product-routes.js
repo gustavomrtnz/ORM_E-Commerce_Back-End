32657fb9fe160b1d8a4be8f01fc973c01c4ea099
Gustavo Martinez <gustavo.mrtnz99@gmail.com>
2024-09-30 17:58:25 -0700

    added delete route in product-routes.js to delete a product by its id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
@@ -34,17 +33,6 @@ router.get('/:id', async (req, res) => {
 } catch (err) {
   res.status(500).json(err);
   }
-
-router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
-
-// get one product
-router.get('/:id',async  (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-
 });
 
 // create new product
@@ -124,8 +112,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
